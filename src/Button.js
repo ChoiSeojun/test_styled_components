240ef8ca784ddd4469aa5a4be210a5bca23c1960
ChoiSeojun <seojun117@gmail.com>
2020-01-15 16:45:25 +0900

    create button

---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 7rem;
+  background: #449d44;
+  border: 2px solid #398439;
+  text-align: center;
+  color: #ffffff;
+`;
+
+export default Button; //파일명과 동일하게 해주어서 기능을 알 수 있게끔 한다.
